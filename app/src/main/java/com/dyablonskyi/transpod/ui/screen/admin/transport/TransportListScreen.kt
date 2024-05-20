7660254db64eb678673bd001e25e7075058cd9ba
Daniel <96320286+Dany-el@users.noreply.github.com>
2024-05-20 18:24:55 +0300

    clean unnecessary imports

--- app/src/main/java/com/dyablonskyi/transpod/ui/screen/admin/transport/TransportListScreen.kt
+++ app/src/main/java/com/dyablonskyi/transpod/ui/screen/admin/transport/TransportListScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -43,9 +42,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +51,6 @@ import androidx.compose.ui.window.Dialog
 import com.dyablonskyi.transpod.R
 import com.dyablonskyi.transpod.data.local.db.entity.Transport
 import com.dyablonskyi.transpod.data.local.db.entity.TransportType
-import com.dyablonskyi.transpod.ui.screen.admin.route.InsertRouteDialog
 import com.dyablonskyi.transpod.ui.theme.TranspodTheme
 
 @Composable
