3d8fd617de151ab35c4c70c6a7fe694fbb97d217
Daniel <96320286+Dany-el@users.noreply.github.com>
2024-05-26 18:58:45 +0300

    add class to get data from query

--- app/src/main/java/com/dyablonskyi/transpod/data/local/db/entity/Route.kt
+++ app/src/main/java/com/dyablonskyi/transpod/data/local/db/entity/Route.kt
@@ -1,6 +1,7 @@
 package com.dyablonskyi.transpod.data.local.db.entity
 
 import androidx.compose.runtime.Immutable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -11,4 +12,10 @@ data class Route(
     val name: String,
     val routeStart: String,
     val routeEnd: String,
+)
+
+data class RouteDriverCount(
+    @ColumnInfo(name = "id") val routeId: Int,
+    @ColumnInfo(name = "name") val routeName: String,
+    @ColumnInfo(name = "driver_count") val driverCount: Int
 )
